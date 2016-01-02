f1d57dba024cafbd009e95c9d30251fca5b91cbe
baybora <baybora.oren@gmail.com>
2016-01-02 17:04:41 +0200

    remove test toast message

--- androidLibrary/src/main/java/com/udacity/gradle/builditbigger/android/library/JokeViewActivity.java
+++ androidLibrary/src/main/java/com/udacity/gradle/builditbigger/android/library/JokeViewActivity.java
@@ -48,9 +48,5 @@ public class JokeViewActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
-    }
-
 
 }
