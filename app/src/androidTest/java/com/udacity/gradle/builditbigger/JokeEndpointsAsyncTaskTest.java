dcfda65061848e7312937cf68abe11b597a81160
baybora <baybora.oren@gmail.com>
2016-01-02 18:13:29 +0200

    add show joke test

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeEndpointsAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeEndpointsAsyncTaskTest.java
@@ -4,24 +4,34 @@ import android.test.AndroidTestCase;
 
 import com.udacity.builditbigger.backend.jokeApi.model.JokeBean;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by baybora on 1/2/16.
  */
 public class JokeEndpointsAsyncTaskTest extends AndroidTestCase implements BaseView{
 
+    private CountDownLatch countDownLatch = new CountDownLatch(1);
+
     public void testGetJokeBean(){
         JokeEndpointsAsyncTask jokeEndpointsAsyncTask = new JokeEndpointsAsyncTask(this);
+        jokeEndpointsAsyncTask.execute();
+        try {
+
+            countDownLatch.await(30, TimeUnit.SECONDS);
+
+        } catch (InterruptedException e) {
+            JokeBean jokeBean = new JokeBean();
+            jokeBean.setErrorMesage(e.getMessage());
+            showErrorMessage(jokeBean);
+        }
+
     }
 
     @Override
     public void showJoke(JokeBean jokeBean) {
-
-        if(jokeBean.getErrorMesage().isEmpty()){
-            assertNotNull("Joke Text received...", jokeBean);
-        }else{
-            assertFalse("Joke Text received...", false);
-        }
-
+        assertNotNull("Joke Text received...", jokeBean.getJokeData());
     }
 
     @Override
